269dfe52eb3a5b2f2b5beee844e3315a84cbbb87
Om Patel <ompat@MacBook-Air-242.local>
2025-12-14 23:40:44 -0600

    external database

--- Final_Project_Om_opate22/settings/production.py
+++ Final_Project_Om_opate22/settings/production.py
@@ -3,3 +3,14 @@ from .base import *
 DEBUG = False
 
 ALLOWED_HOSTS = ['ompatel277.pythonanywhere.com',  '127.0.0.1', 'localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ompatel277$info390finalmg-msql-database',
+        'USER': 'ompatel277',
+        'PASSWORD': 'graingerlibrary',
+        'HOST': 'ompatel277.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+    }
+}
